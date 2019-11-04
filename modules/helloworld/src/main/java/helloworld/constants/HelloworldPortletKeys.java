c5c0dcbb0de691cf8ded411b26b84a47b4f15ce4
rakshansh.jain <rakshansh.jain@teamcomputers.com>
2019-11-04 11:55:31 +0530

    first commit

---
+++ modules/helloworld/src/main/java/helloworld/constants/HelloworldPortletKeys.java
@@ -0,0 +1,11 @@
+package helloworld.constants;
+
+/**
+ * @author 10608
+ */
+public class HelloworldPortletKeys {
+
+	public static final String HELLOWORLD =
+		"helloworld_HelloworldPortlet";
+
+}
