6bfa3e027bbbc3d6d8348c17bdb7f9bcc87966ea
rakshansh.jain <rakshansh.jain@teamcomputers.com>
2019-11-07 08:57:37 +0530

    New demo project for DevOps setup testing.

---
+++ modules/Demo/src/main/java/Demo/constants/DemoPortletKeys.java
@@ -0,0 +1,11 @@
+package Demo.constants;
+
+/**
+ * @author 10608
+ */
+public class DemoPortletKeys {
+
+	public static final String DEMO =
+		"Demo_DemoPortlet";
+
+}
---
+++ modules/Demo/src/main/resources/META-INF/resources/view.jsp
@@ -0,0 +1,7 @@
+<%@ include file="/init.jsp" %>
+
+<p>
+	<b><liferay-ui:message key="demo.caption"/></b>
+	
+	Here we are testing DevOps setup!!!
+</p>
